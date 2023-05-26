83e1bb6c766fe30985fbc01ff483307bdd79e535
Bill Sideris <bill88t@feline.gr>
2023-05-27 00:10:20 +0300

    Improvements to cut & text

--- pytherm.py
+++ pytherm.py
@@ -7,21 +7,22 @@ class thermal:
         self.regular()
 
     def cut(self, lines=3):
-        self._device.cut()
+        self._device._raw(b"\n" * lines + b"\x1dV\x00")
+        self._device.flush()
 
     def line(self, count=1):
         self._device.text("\n" * count)
 
     def write(self, data="", end="\n"):
         if self._device is not None:
-            self._device.text(data + end)
+            self._device._raw(bytes(data + end, "cp437"))
 
     def cat(self, filename):
         if self._device is not None:
             with open(filename, "r") as f:
                 data = f.read()
                 self.write(data, end="")
-
+                self._device.flush()
 
     def left(self):
         self._device.text("\x1b\x61\x00")
@@ -40,8 +41,10 @@ class thermal:
 
     def qr(self, content, ec=2, size=10):
         self._device.qr(content, ec, size)
+        self._device.flush()
 
     def image(self, path):
         self._device.image(path)
+        self._device.flush()
 
 
